25b0790658c9d06b67837a2ab0ebd9e250f2386b
Maria Öhrman <maria.ohrman@gu.se>
2021-10-12 16:25:11 +0200

    lots of refactoring

--- backend.py
+++ backend.py
@@ -108,7 +108,9 @@ def main():
         'customAttributes': {},
     }
 
-    for mode, settings in read_files():
+    # this loop checks default settings values, moves mode and folder info to corpus level and 
+    # extracts and removes all attributes from corpus level
+    for mode, settings in _read_files():
         modes[mode] = {}
         for key in settings.keys():
             if key != 'corpora':
@@ -133,7 +135,7 @@ def main():
             # first we must see if the attributes has been used before
             for attr_type, attrs in [("attributes", p_attrs), ("structAttributes", s_attrs), ("customAttributes", c_attrs)]:
                 for attr_key, attr in attrs.items():
-                    check_attribute(attributes[attr_type], corpus_id, attr_key, attr)
+                    _check_attribute(attributes[attr_type], corpus_id, attr_key, attr)
 
             modeFolder = {
                 "name": mode,
@@ -146,13 +148,20 @@ def main():
                 corpusSettings["mode"] = [modeFolder]
                 corpora[corpus_id] = corpusSettings
 
-    # TODO sorts keys, but can be fixed
+    # first give each attribute a unqiue name
+    attributes = _create_unique_names(attributes)
 
-    # handles attributes unique to a corpus or attributes that can be grouped
-    groups = _fix_attributes(corpora, attributes)
+    # inline attributes that are unique to a corpora
+    _inline_attributes(corpora, attributes)
 
-    # gives every attribute a unique identifier and adds identifiers to corpora, removes corpora from attributes
-    _fix_attributes2(corpora, attributes)
+    # create groups of attributes that are commonly used together
+    groups = _create_groups(corpora, attributes)
+
+    # adds identifiers to corpora, removes corpora from attributes
+    _add_attributes_to_corpora(corpora, attributes)
+
+
+    # TODO yaml sorts keys, but can be fixed
 
     print("write groups", len(groups.keys()))
     for group_name in groups.keys():
@@ -183,15 +192,15 @@ def main():
             yaml.dump(corpus, stream=fp, allow_unicode=True)
 
 
-def check_attribute(container, corpus_id, attr_key, attr):
+def _check_attribute(attributes, corpus_id, attr_key, attr):
     attr['id'] = attr_key
     source = corpus_id
-    if attr_key not in container:
+    if attr_key not in attributes:
         # first find of this attribute
-        container[attr_key] = []
+        attributes[attr_key] = []
 
     found = False
-    for content in container[attr_key]:
+    for content in attributes[attr_key]:
         attr_alt = content['attribute']
         corpora = content['corpora']
         if attr_alt == attr:
@@ -201,7 +210,7 @@ def check_attribute(container, corpus_id, attr_key, attr):
             found = True
             break
     if not found:
-        container[attr_key].append({"attribute": attr, "corpora": [source]})
+        attributes[attr_key].append({"attribute": attr, "corpora": [source]})
 
 
 def parse_folders(outer_folders):
@@ -231,75 +240,26 @@ def parse_folders(outer_folders):
     return new_corpora_folders, corpus_to_folder
 
 
-def read_files():
+def _read_files():
     for filepath in glob.glob('./interpreted/*.json'):
         with open(filepath, 'r') as fp:
             yield filepath.split('/')[-1].split('.')[0], json.load(fp)
 
 
-def _fix_attributes(corpora_settings, attributes_type_settings):
+def _create_unique_names(global_attributes):
     """
-    move all attributes that are only in one corpus into the actual corpus definition
-    
-
-    find common sets of settings that can be used in many corpora (modernAttrs, commonStructAttrs)
-
-    attributes_settings has format
-    attr_name: [ { attribute: <attribute setting>, corpora: [ "<mode>#<corpus>#<attributes/structAttributes>", ... ]}  ]
+    give each attribute definition a unique name
     """
-
-    # corpora, sorted and joined with ',' a list of attributes
-    corpusset_to_attributes = {}
-    for attr_type, attributes_settings in attributes_type_settings.items():
+    new_attributes = {'attributes': {}, 'structAttributes': {}, 'customAttributes': {}}
+    for attr_type, attributes_settings in global_attributes.items():
         for attr_key, attribute_settings in attributes_settings.items():
-            delete_idx = []
-            for idx, attribute_setting in enumerate(attribute_settings):
-                corpora = attribute_setting["corpora"]
-                attribute = attribute_setting["attribute"]
-                
-                if len(corpora) == 1:
-                    # is unique attribute, just inline in corpus definintion
-                    corpus = corpora[0]
-                    asdf = corpora_settings[corpus].get(attr_type, {})
-                    asdf[attr_key] = attribute
-                    corpora_settings[corpus][attr_type] = asdf
-
-                    delete_idx.append(idx)       
-                else:
-                    # code to help find groups of attributes
-
-                    # cannot find any modernAttrs :( however lots of other nice groups
-                    corpora_group = ",".join(sorted(corpora))
-                    attributes_for_corpusset = corpusset_to_attributes.get(corpora_group, {'attributes': {}, 'structAttributes': {}, 'customAttributes': {}})
-                    attributes_for_corpusset[attr_type][attribute["id"]] = attribute
-                    corpusset_to_attributes[corpora_group] = attributes_for_corpusset
-
-            # remove moved attributes after loop
-            for idx in reversed(delete_idx):
-                del attribute_settings[idx]
-    
-    for attr_key in list(attributes_settings.keys()):
-        if len(attributes_settings[attr_key]) == 0:
-            del attributes_settings[attr_key]
-    
-    return _create_groups(corpora_settings, corpusset_to_attributes, attributes_type_settings)
-
-
-def _fix_attributes2(corpora_settings, attributes_type_settings):
-    """
-    for each attribute in attribute_type_settings, if there are more than one attribute, generate a new name
-    then add all attributes to the referecing corpora
-    then remove information about corpora from attributes_type_settings
-    """
-    for attr_type in attributes_type_settings.keys():
-        for attr_key in list(attributes_type_settings[attr_type].keys()):
-            attributes = attributes_type_settings[attr_type][attr_key]
             # name -> annotation in backend corpora
             # id -> only used to separate attributes in frontend config
-            if len(attributes) > 1:
-                # must find new ID
-                tmp = sorted([(len(thing['attribute'].keys()), thing) for thing in attributes], key=lambda thingWithSize: thingWithSize[0], reverse=True)
+            if len(attribute_settings) > 1:
+                # must find new ID, trying to sort large -> small attributes
+                tmp = sorted([(len(thing['attribute'].keys()), thing) for thing in attribute_settings], key=lambda x: x[0], reverse=True)
 
+                # this is a bad attempt to find a good name 
                 if len(tmp) == 2:
                     biggest_attr = tmp[0][1]
                     smallest_attr = tmp[1][1]
@@ -313,74 +273,110 @@ def _fix_attributes2(corpora_settings, attributes_type_settings):
                         thing['attribute']['name'] = attr_key
             else:
                 # use attr_key as id and name
-                attributes[0]['attribute']['id'] = attr_key
-                attributes[0]['attribute']['name'] = attr_key
+                attribute_settings[0]['attribute']['id'] = attr_key
+                attribute_settings[0]['attribute']['name'] = attr_key
 
-            # remove old object
-            del attributes_type_settings[attr_type][attr_key]
+            for attribute_setting in attribute_settings:
+                corpora = attribute_setting["corpora"]
+                attribute = attribute_setting["attribute"]
 
-            for thing in attributes:
-                # add to corpora
-                attribute = thing['attribute']
-                corpora = thing['corpora']
-                for corpus in corpora:
-                    refs = corpora_settings[corpus].get(attr_type + 'Ref', [])
-                    refs.append(attribute['id'])
-                    corpora_settings[corpus][attr_type + 'Ref'] = refs
+                # save corpora on attribute, to be used later for grouping and also since the 
+                # attributes in some way must be added back on corpus configuration
+                attribute['corpora'] = corpora
+                new_attributes[attr_type][attribute['id']] = attribute
 
-                # fix data in attribute settings
-                attributes_type_settings[attr_type][attribute['id']] = attribute
+    return new_attributes
+
+def _inline_attributes(corpora_settings, global_attributes):
+    """
+    move all attributes that are only in one corpus into the actual corpus definition
+    """
+    for attr_type, attributes in global_attributes.items():
+        for attr_key in list(attributes.keys()):
+            attribute = attributes[attr_key]
+            corpora = attribute["corpora"]
+
+            if len(corpora) == 1:
+                # is unique attribute, just inline in corpus definintion
+                corpus = corpora[0]
+                attrs = corpora_settings[corpus].get(attr_type, {})
+                attrs[attr_key] = attribute
+                corpora_settings[corpus][attr_type] = attrs
+                del attributes[attr_key]
 
 
-def _create_groups(corpora_settings, corpusset_to_attributes, attributes_type_settings):
+def _create_groups(corpora_settings, global_attributes):
+    """
+    find common sets of settings that can be used in many corpora (modernAttrs, commonStructAttrs)
+    """
+
+    # corpora, sorted and joined with ',' a list of attributes
+    group_to_attributes = {}
+    for attr_type, attributes in global_attributes.items():
+        for attr_key, attribute in attributes.items():
+            corpora = attribute["corpora"]
+            if len(corpora) != 1:
+                # code to help find groups of attributes
+
+                # cannot find any modernAttrs :( however lots of other nice groups
+                corpora_group = ",".join(sorted(corpora))
+                attributes_for_corpusset = group_to_attributes.get(corpora_group, {'attributes': {}, 'structAttributes': {}, 'customAttributes': {}})
+                attributes_for_corpusset[attr_type][attribute["id"]] = attribute
+                group_to_attributes[corpora_group] = attributes_for_corpusset
+
     groups = {}
-    for corpora in corpusset_to_attributes.keys():
-        attributes_for_corpusset = corpusset_to_attributes[corpora]
+    for corpora in group_to_attributes.keys():
+        group_attributes = group_to_attributes[corpora]
         create_group = False
-        for attr_type, attr_type_content in attributes_for_corpusset.items():
-            if len(attr_type_content.keys()) > 1:
+        attribute_count = 0
+        for attr_type, attr_type_content in group_attributes.items():
+            attribute_count += len(attr_type_content.keys())
+            if attribute_count > 1:
                 create_group = True
                 break
+
         if create_group:
+                    
             actual_corpora = corpora.split(',')
             # group naming scheme: name of first corpus in group, then number of corpora in group
             # should be improved later, of course
             group_name = actual_corpora[0] + '_' + str(len(actual_corpora))
             while group_name in groups:
-                print("group name already used!", group_name)
+                # group name already used!
                 group_name = actual_corpora[0] + '_' + actual_corpora[1] + '_' + str(len(actual_corpora))
+
             for corpus in actual_corpora:
                 inherits = corpora_settings[corpus].get("inherits", [])
                 inherits.append(group_name)
                 corpora_settings[corpus]["inherits"] = inherits
-            _remove_groups_from_attributes(actual_corpora, attributes_for_corpusset, attributes_type_settings)
-            groups[group_name] = attributes_for_corpusset
+            
+            for attr_type, attributes in group_attributes.items():
+                for attr_key in attributes.keys():
+                    del attributes[attr_key]["corpora"]
+                    del global_attributes[attr_type][attr_key]
+
+            groups[group_name] = group_attributes
 
     return groups
+
+
+def _add_attributes_to_corpora(corpora_settings, global_attributes):
+    """
+    add all attributes to the referecing corpora
+    then remove information about corpora from global_attributes
+    """
+    for attr_type, attributes in global_attributes.items():
+        for attr_key, attribute in attributes.items():
+            corpora = attribute.pop('corpora')
+            
+            if attr_key != attribute['id']:
+                raise AssertionError('something is wrång')
+
+            for corpus in corpora:
+                refs = corpora_settings[corpus].get(attr_type + 'Ref', [])
+                refs.append(attr_key)
+                corpora_settings[corpus][attr_type + 'Ref'] = refs
     
-
-def _remove_groups_from_attributes(corpora, group_attributes, attributes_type_settings):
-    """
-    removes the attributes that have been moved to groups from the global attributes file    
-    """
-    for attr_type in group_attributes.keys():
-        attr_settings = attributes_type_settings[attr_type]
-        for attr_key in group_attributes[attr_type].keys():
-            remove_idx = None
-            for idx, attr_setting in reversed(list(enumerate(attr_settings[attr_key]))):
-                if sorted(attr_setting['corpora']) == sorted(corpora):
-                    remove_idx = idx
-                    break
-            if remove_idx != None:
-                del attr_settings[attr_key][remove_idx]
-            else:
-                raise RuntimeError("Not found", corpora, attr_type, attr_key)
-        
-        # remove now empty attributes list
-        for attr_key in list(attr_settings.keys()):
-            if len(attr_settings[attr_key]) == 0:
-                del attr_settings[attr_key]
-
 
 if __name__ == '__main__':
     main()
